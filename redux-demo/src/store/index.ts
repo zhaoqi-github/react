31ce5cb96de5f2260cff50856dbe657eb8c229f8
zhaoqi <abby.zhao@biomind.ai>
2023-05-31 17:19:16 +0800

    feat: 实现简单加减

--- redux-demo/src/store/index.ts
+++ redux-demo/src/store/index.ts
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import countReduce from './count';
 
+export type StateType = {
+  count: Number
+}
+
 export default configureStore({
   reducer: {
     count: countReduce,
